eab1584c67aa36ad72643ce45e1222d0fb26627e
norbert <norbert.madauss@gmail.com>
2022-07-20 11:04:06 +0200

    setup app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>My portfolio</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
