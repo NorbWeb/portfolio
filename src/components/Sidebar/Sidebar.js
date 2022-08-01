8fab4f5bd8d32926d25c1580fcc94654f68d01d7
Norbert <norbert.madauss@gmail.com>
2022-08-01 17:28:00 +0200

    change logo

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,7 +1,8 @@
 import { Link, NavLink } from 'react-router-dom'
 import './Sidebar.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo-sub.png'
+// import LogoSubtitle from '../../assets/images/logo-sub.png'
+import LogoName from '../../assets/images/name.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faEnvelope,
@@ -20,7 +21,7 @@ const Sidebar = () => {
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+        <img className="sub-logo" src={LogoName} alt="logo-subtitle" />
       </Link>
       <nav>
         <NavLink exact="true" activeclassname="active" to="/">
