c24fc4f73c5a19353b9e06d2bf3f7b721b24b436
Norbert <norbert.madauss@gmail.com>
2022-08-03 08:15:57 +0200

    uninstall gsap & fix logo animation

--- src/components/Home/Logo/Logo.js
+++ src/components/Home/Logo/Logo.js
@@ -1,37 +1,11 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import LogoN from '../../../assets/images/letter-n.png'
 import './Logo.scss'
 
 const Logo = () => {
-  const bgRef = useRef()
-  const solidLogoRef = useRef()
-
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap.timeline().to(bgRef.current, {
-      duration: 1,
-      opacity: 1,
-    })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
 
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img ref={solidLogoRef} className="solid-logo" src={LogoN} alt="S" />
+    <div className="logo-container" >
+      <img  className="solid-logo" src={LogoN} alt="N" />
     </div>
   )
 }
