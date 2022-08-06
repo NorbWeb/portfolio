919cb2f7791a06d109eba8887e22937b332e92e1
Norbert <norbert.madauss@gmail.com>
2022-08-06 15:22:30 +0200

    fix responsive design for layout, home + add to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -29,13 +29,12 @@ const Portfolio = () => {
                 className="portfolio-image"
                 alt={port.title}
               />
-              <div className='content'>
-                <p className='title'>{port.title}</p>
-                <h4 className='description'>{port.description}</h4>
-                <button
-                className='btn'
-                onClick={()=> window.open(port.url)}
-                >view</button>
+              <div className="content">
+                <p className="title">{port.title}</p>
+                <h4 className="description">{port.description}</h4>
+                <button className="btn" onClick={() => window.open(port.url)}>
+                  view
+                </button>
               </div>
             </div>
           )
