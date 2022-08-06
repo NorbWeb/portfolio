9e31eb9d879edae75953e9c231053d5e88b89890
Norbert <norbert.madauss@gmail.com>
2022-08-06 14:06:07 +0200

    add responsive design to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -24,7 +24,7 @@ const About = () => {
 
   return (
     <>
-      <div className="container about-page">
+      <div className="container about-page" style={{transform: "matrix(1,0,0,1,0,0)"}}>
         <div className="text-zone">
           <h1>
             <AnimatedLetters
@@ -78,7 +78,6 @@ const About = () => {
               <FontAwesomeIcon
                 icon={faNodeJs}
                 color="#215732"
-                style={{ rotate: '270deg' }}
               />
             </div>
           </div>
