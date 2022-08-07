04bee89a1566b70d5e4cb3bc496889a6d5472dd2
Norbert <norbert.madauss@gmail.com>
2022-08-07 17:39:00 +0200

    some changes

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,11 +2,10 @@ import React, { useEffect, useState } from 'react'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 import './Portfolio.scss'
-import porfolioData from '../../data/portfolio.json'
+import RenderPortfolio from './Projects/Projects'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
-  console.log(porfolioData)
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -18,44 +17,17 @@ const Portfolio = () => {
     }
   })
 
-  const renderPortfolio = (portfolio) => {
-    return (
-      <div className="images-container">
-        {portfolio.map((port, index) => {
-          return (
-            <div key={index} className="image-box">
-              <img
-                src={port.cover}
-                className="portfolio-image"
-                alt={port.title}
-              />
-              <div className="content">
-                <p className="title">{port.title}</p>
-                <h4 className="description">{port.description}</h4>
-                <button className="btn" onClick={() => window.open(port.url)}>
-                  view
-                </button>
-              </div>
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-
   return (
     <>
       <div className="container portfolio-page">
-        <div className='text-zone'>
-        <h1 className="page-title">
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={'Portfolio'.split('')}
-            idx={15}
-          />
-        </h1>
-        </div>
-        {renderPortfolio(porfolioData.portfolio)}
+          <h1 className="page-title">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'Portfolio'.split('')}
+              idx={15}
+            />
+          </h1>
+        <RenderPortfolio />
       </div>
       <Loader type="pacman" />
     </>
