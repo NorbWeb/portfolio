fea54d42b0d2bc4fd948e6c6a2523f189c6943a6
Norbert <norbert.madauss@gmail.com>
2022-08-10 16:51:55 +0200

    add info text to projects

--- src/components/Portfolio/Projects/Projects.js
+++ src/components/Portfolio/Projects/Projects.js
@@ -18,6 +18,8 @@ const RenderPortfolio = () => {
             <div className="content">
               <p className="title">{port.title}</p>
               <h4 className="description">{port.description}</h4>
+              <p className='info'>{port.info}</p>
+
               <button className="btn" onClick={() => window.open(port.url)}>
                 view
               </button>
