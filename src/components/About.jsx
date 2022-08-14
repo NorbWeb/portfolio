31f11bfa89360e747163e96cbb457c3ead4529a1
Norbert <norbert.madauss@gmail.com>
2022-08-14 13:34:46 +0200

    add animation to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+  faNodeJs,
+} from '@fortawesome/free-brands-svg-icons'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from './AnimatedLetters'
@@ -48,7 +57,31 @@ const About = () => {
           </p>
         </div>
         <div className="box-effect">
-          <h3>Placeholder</h3>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon
+                icon={faNodeJs}
+                color="#215732"
+              />
+            </div>
+          </div>
+        </div>
         </div>
       </div>
     </div>
