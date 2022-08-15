d8b72b8454823b70f6e34e1ffbcb9a81ebdb4718
Norbert <norbert.madauss@gmail.com>
2022-08-15 17:38:13 +0200

    add portfolio & contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,39 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import AnimatedLetters from './AnimatedLetters'
+import emailjs from '@emailjs/browser'
+import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet'
+import './Styles/Contact.scss'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 1500)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'service_bnrwg44',
+        'template_xrtjlfe',
+        refForm.current,
+        'gJKt689dLpsgkIsZ5'
+      )
+      .then(
+        () => {
+          alert('Message successfully send!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <div className="box-contact">
@@ -21,9 +46,55 @@ const Contact = () => {
               idx={1}
             />
           </h1>
+          <p>
+            Do you have some questions about coding, my work or myself? Do you
+            want to work with me or have some interesting ideas? Then just
+            contact me! I am looking forward to hearing from you. And if you
+            have a job offer, don't hesitate and hit the keys!{' '}
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <div className="form-top">
+                <input type="text" name="name" placeholder="Name" required />
+                <input type="email" name="email" placeholder="Email" required />
+              </div>
+              <div className='form-bottom'>
+                <input
+                  placeholder="Subject"
+                  type="text"
+                  name="subject"
+                  required
+                />
+                <textarea
+                  placeholder="Message"
+                  name="message"
+                  required
+                ></textarea>
+                <input type="submit" className="submit-button" value="SEND" />
+              </div>
+            </form>
+          </div>
         </div>
         <div className="box-effect">
-          <h3>Placeholder</h3>
+          <div className="info-map">
+            <span>
+              Norbert Madauß,
+              <br />
+              Germany,
+              <br />
+              Somewhere in Potsdam
+              <br />
+            </span>
+            <span className="span-email">norbert.madauss@gmail.com</span>
+          </div>
+          <div className="map-wrap">
+            <MapContainer center={[52.394919, 13.060529]} zoom={13}>
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker position={[52.394919, 13.060529]}>
+                <Popup className="popup">This is NOT my position 😉</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import AnimatedLetters from './AnimatedLetters'
+import porfolioData from '../data/portfolio.json'
+import './Styles/Portfolio.scss'
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -21,10 +23,30 @@ const Portfolio = () => {
               idx={1}
             />
           </h1>
+          <div className="images-container">
+          {porfolioData.portfolio.map((port, index) => {
+            return (
+              <div key={index} className="image-box">
+                <img
+                  src={port.cover}
+                  className="portfolio-image"
+                  alt={port.title}
+                />
+                <div className="content">
+                  <p className="title">{port.title}</p>
+                  <h4 className="description">{port.description}</h4>
+                  <p className="info">{port.info}</p>
+
+                  <button className="btn" onClick={() => window.open(port.url)}>
+                    view
+                  </button>
+                </div>
+              </div>
+            )
+          })}
         </div>
-        <div className="box-effect">
-          <h3>Placeholder</h3>
         </div>
+        
       </div>
     </div>
   )
