03f26d5ef684e536f848edacc99fa5c4e75017c7
Norbert <norbert.madauss@gmail.com>
2022-08-20 13:09:50 +0200

    logo animation works on desktop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,22 +14,22 @@ const Home = () => {
     }, 4000)
   }, [])
 
-  // const arr =
-  //   '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36'.split(
-  //     ' '
-  //   )
-
   const arr = []
   let squareRoot = 0
   const tileNumber = (num) => {
     for (let i = 1; i <= num; i++) {
-      arr.push(i)
+      if (Math.random() < 0.5) {
+        arr.push(`${i} x`)
+      } else {
+        arr.push(`${i} y`)
+      }
+
       squareRoot = Math.sqrt(num)
     }
-    return arr, squareRoot
+    return arr + squareRoot
   }
 
-  tileNumber(9)
+    tileNumber(64)
 
   return (
     <div className="box-home">
